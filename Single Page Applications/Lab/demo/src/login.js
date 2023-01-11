900215d6cc820bf184816bbac1a9d5df0a228433
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-01-11 10:03:23 +0200

    Add login.js

---
+++ Single Page Applications/Lab/demo/src/login.js
@@ -0,0 +1,29 @@
+import { createSubmitHandler } from "./helper.js";
+import { post } from "./api.js";
+
+const section = document.getElementById('loginView');
+const form = section.querySelector('form');
+createSubmitHandler(form, onSubmit);
+section.remove();
+
+let ctx = null;
+
+export function showLogin(inCtx) {
+    ctx = inCtx;
+    ctx.render(section);
+}
+
+async function onSubmit({ email, password }) {
+
+
+    const data = await post('/users/login', { email, password });
+
+    const userData = {
+        email: data.email,
+        accessToken: data.accessToken,
+        id: data._id
+    }
+    sessionStorage.setItem('userData', JSON.stringify(userData));
+    ctx.checkUserNav();
+    ctx.goTo('homeBtn');
+}
