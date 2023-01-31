e6839931ae5f16aac6da46cbcdadf63ea3c88bae
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-01-31 14:31:43 +0200

    Add template.js

---
+++ Client Side Rendering/Exercise/01.List-Towns/template.js
@@ -0,0 +1,7 @@
+import { html } from './node_modules/lit-html/lit-html.js';
+
+export const createUl = (towns) => html`
+   <ul>${towns.map(createLi)}</ul>`;
+
+const createLi = (town) => html`
+    <li>${town}</li>`;
