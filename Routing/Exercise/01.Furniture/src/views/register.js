c87a123927c9999c011a392c6b0b77af6ec43bc0
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-02-08 10:41:04 +0200

    Add register.js

---
+++ Routing/Exercise/01.Furniture/src/views/register.js
@@ -0,0 +1,32 @@
+import { register } from "../api/user.js";
+import { registerTemplate } from "../template.js";
+
+
+export function registerPage(ctx) {
+    ctx.render(registerTemplate());
+    const form = document.querySelector('form');
+    form.addEventListener('submit', onSubmit);
+
+
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const rePass = formData.get('rePass').trim();
+
+        if (email == '' || password == '') {
+            return alert('All fields are required!');
+        }
+        if (password != rePass) {
+            return alert('Passwords don\'t match!');
+        }
+
+        await register({ email, password });
+        form.reset();
+        ctx.updateNavBar();
+        ctx.page.redirect('/');
+    }
+}
