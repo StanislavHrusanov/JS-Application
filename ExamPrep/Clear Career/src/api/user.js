51f3378dbeb431086779dac4f50f32c41799b8ed
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-02-13 09:45:00 +0200

    Add user.js

---
+++ ExamPrep/Clear Career/src/api/user.js
@@ -0,0 +1,25 @@
+import { get, post } from "./api.js"
+
+const endpoints = {
+    'login': '/users/login',
+    'register': '/users/register',
+    'logout': '/users/logout'
+}
+
+export async function login(data) {
+    const user = await post(endpoints.login, data);
+
+    sessionStorage.setItem('user', JSON.stringify(user));
+}
+
+export async function register(data) {
+    const user = await post(endpoints.register, data);
+
+    sessionStorage.setItem('user', JSON.stringify(user));
+}
+
+export function logout() {
+    get(endpoints.logout);
+
+    sessionStorage.removeItem('user');
+}
