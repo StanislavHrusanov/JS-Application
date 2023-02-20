91c844c074a0cb3f998c6650fcd74a32b2cfe183
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-02-20 11:23:55 +0200

    Add editPostPageTemplate.js

---
+++ ExamPrep/Local Orphanages/src/templates/editPostPageTemplate.js
@@ -0,0 +1,35 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+export const editPostPageTemplate = (onSubmit, postDetails) => html`
+<section id="edit-page" class="auth">
+    <form @submit=${onSubmit} id="edit">
+        <h1 class="title">Edit Post</h1>
+
+        <article class="input-group">
+            <label for="title">Post Title</label>
+            <input type="title" name="title" id="title" value="${postDetails.title}">
+        </article>
+
+        <article class="input-group">
+            <label for="description">Description of the needs </label>
+            <input type="text" name="description" id="description" value="${postDetails.description}">
+        </article>
+
+        <article class="input-group">
+            <label for="imageUrl"> Needed materials image </label>
+            <input type="text" name="imageUrl" id="imageUrl" value="${postDetails.imageUrl}">
+        </article>
+
+        <article class="input-group">
+            <label for="address">Address of the orphanage</label>
+            <input type="text" name="address" id="address" value="${postDetails.address}">
+        </article>
+
+        <article class="input-group">
+            <label for="phone">Phone number of orphanage employee</label>
+            <input type="text" name="phone" id="phone" value="${postDetails.phone}">
+        </article>
+
+        <input type="submit" class="btn submit" value="Edit Post">
+    </form>
+</section>`;
