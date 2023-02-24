cb370e629e9940fab1bbab5e354e89261241c3c9
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-02-24 12:26:01 +0200

    Add util

---
+++ ExamPrep/Meme Lounge/src/util.js
@@ -0,0 +1,25 @@
+import { showNotification } from "./notifications.js";
+
+export function getUser() {
+    return JSON.parse(localStorage.getItem('user'));
+}
+
+export function getFormDataAsObj(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const dataAsObj = Object.fromEntries(formData.entries());
+
+    for (let key in dataAsObj) {
+        const currField = dataAsObj[key];
+        currField.trim();
+
+        if (currField == '') {
+            showNotification('All fields are required!');
+            throw new Error('All fields are required!');
+        }
+    }
+
+    return dataAsObj;
+}
