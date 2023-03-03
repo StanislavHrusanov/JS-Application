f5354f18e30abfd7998cc12a6c473a1cd6532c8c
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-03-03 15:15:39 +0200

    Add register view

---
+++ ExamPrep/MyExam/src/views/register.js
@@ -0,0 +1,29 @@
+import { register } from "../api/user.js";
+import { registerTemplate } from "../templates/registerTemplate.js";
+
+export function registerPage(ctx) {
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+        const rePass = formData.get('re-password').trim();
+
+        if (email == '' || password == '') {
+            return alert('All fields are required!');
+        }
+
+        if (password != rePass) {
+            return alert('Passwords don\'t match!');
+        }
+
+        await register({ email, password });
+        e.target.reset();
+        ctx.updateNavBar();
+        ctx.page.redirect('/dashboard');
+    }
+}
